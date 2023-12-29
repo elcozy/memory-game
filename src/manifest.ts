b3fc76f7262d1f09228027ea9b52946f0acfe101
Chiemeka Madufor <61386343+elcozy@users.noreply.github.com>
2023-12-29 16:11:28 +0100

    updated icons

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,44 +1,45 @@
 import Atom from "./assets/gameIcons/Atom.svg";
-import Bahai from "./assets/gameIcons/Bahai.svg";
 import Bell from "./assets/gameIcons/Bell.svg";
 import Bomb from "./assets/gameIcons/Bomb.svg";
-import Bookmark from "./assets/gameIcons/Bookmark.svg";
+import Truck from "./assets/gameIcons/Truck.svg";
 import Brain from "./assets/gameIcons/Brain.svg";
 import Bug from "./assets/gameIcons/Bug.svg";
 import Car from "./assets/gameIcons/Car.svg";
 import Cloud from "./assets/gameIcons/Cloud.svg";
-import CodeBranch from "./assets/gameIcons/CodeBranch.svg";
+import Code from "./assets/gameIcons/Code.svg";
 import Coffee from "./assets/gameIcons/Coffee.svg";
-import DiceFive from "./assets/gameIcons/DiceFive.svg";
+import Dice from "./assets/gameIcons/Dice.svg";
 import Dragon from "./assets/gameIcons/Dragon.svg";
 import Fire from "./assets/gameIcons/Fire.svg";
 import Hamburger from "./assets/gameIcons/Hamburger.svg";
 import Lemon from "./assets/gameIcons/Lemon.svg";
 import Meteor from "./assets/gameIcons/Meteor.svg";
 import Sun from "./assets/gameIcons/Sun.svg";
+import Plane from "./assets/gameIcons/Plane.svg";
+import Wifi from "./assets/gameIcons/Wifi.svg";
 import Title from "./assets/Title.svg";
 import MemoryIcon from "./assets/memory.svg";
 
 const svgIcons = [
     Atom,
-    Bahai,
     Bell,
     Bomb,
-    Bookmark,
+    Truck,
     Brain,
     Bug,
     Car,
     Cloud,
-    CodeBranch,
+    Code,
     Coffee,
-    DiceFive,
+    Dice,
     Dragon,
     Fire,
     Hamburger,
     Lemon,
     Meteor,
+    Plane,
     Sun,
-    Sun,
+    Wifi,
 ];
 
 const arrayOfObjects = svgIcons.map((src) => ({
